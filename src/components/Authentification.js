36f78a937f50ee04e935e6b5301f436759bd4a00
Bradley_Carson <bmcarson1671@gmail.com>
2017-10-04 09:35:39 -0400

    protected route working, need redirect

---
+++ src/components/Authentification.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+function authRequired(ComposedComponent){
+  console.log('auth required called');
+  class Authentication extends Component{
+    isLoggedIn(){
+      console.log('function running');
+      let user = localStorage.getItem('user')
+
+      if(!user){
+        console.log('returned false');
+        return false;
+      }
+
+      var currentUser = JSON.parse(user);
+      return !!currentUser.sessionToken;
+    }
+
+    componentWillMount(){
+      console.log('Is Logged In: ', !this.isLoggedIn());
+      !this.isLoggedIn() ? this.props.history.push('/') : null;
+    }
+
+    render(){
+      console.log('testing redner return');
+      return this.isLoggedIn() ? <ComposedComponent {...this.props} /> : null;
+    }
+  }
+
+  return Authentication;
+}
+
+export default authRequired;
--- src/layouts/Base.js
+++ src/layouts/Base.js
@@ -3,13 +3,14 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Main from '../components/Main.js';
 import Login from '../components/Login.js';
+import authRequired from '../components/Authentification.js';
 
 function BaseLayout(props){
   return(
     <Router>
         <main>
           <Route path='/' exact component={Login} />
-          <Route path='/home' exact component={Main} />
+          <Route path='/home' exact component={authRequired(Main)} />
         </main>
     </Router>
 );
